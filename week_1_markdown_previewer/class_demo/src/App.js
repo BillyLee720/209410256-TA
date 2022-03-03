5b45022744b52d83a16e8a6b854a931a1a1466ea
BillyLee720 <209410256@gms.tku.edu.tw>
2022-03-03 21:52:46 +0800

    0303 TA

--- week_1_markdown_previewer/class_demo/src/App.js
+++ week_1_markdown_previewer/class_demo/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 function App() {
-  
+  const [markdown, setMarkdown] = useState('由此處開始填寫 Markdown')
   return (
     <main>
-      <h1 className="title">Markdown Preview</h1>
       <section className="markdown">
-        <textarea className="input"></textarea>
-        <article></article>
+        <textarea className="input" value={markdown} onChange={(e) => setMarkdown(e.target.value)}></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
       </section>
     </main>
   )
